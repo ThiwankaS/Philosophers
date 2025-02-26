9c45b2f1918610187f9a92ca38b878a252c57cd7
Thiwanka Somachandra <thivankas@outlook.com>
2025-02-26 17:41:48 +0200

    Parsing arguments and handel return values from threads

--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -1,30 +1,36 @@
 #include "philosophers.h"
 
-void *f1()
+void *f1(void *arg)
 {
-	while(1)
+	int *iptr = (int *)arg;
+	for(int i = 0; i < 5; i++)
 	{
 		sleep(1);
-		printf("My turn!\n");
+		printf("My   turn {%d}!\n", i);
+		(*iptr)++;
 	}
-	return (NULL);
+	return ((void *)iptr);
 }
 
 void *f2()
 {
-	while(1)
+	for(int i = 0;i < 3; i++)
 	{
-		sleep(2);
-		printf("Your turn!\n");
+		sleep(1);
+		printf("Your turn {%d}!\n", i);
 	}
+	return (NULL);
 }
 
 int main (void)
 {
 	pthread_t thread;
+	int count = 5;
+	int *ptr;
 
-	pthread_create(&thread, NULL, f1, NULL);
+	pthread_create(&thread, NULL, f1, &count);
 	f2();
-	pthread_join(thread, NULL);
+	pthread_join(thread, (void *)&ptr);
+	printf("Return value : %d\n", *ptr);
 	return (0);
 }
